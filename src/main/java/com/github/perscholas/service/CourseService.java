19938c31940d755465a54539f4c3fe559e2970e3
Kevin Wang <bird831205@hotmail.com>
2020-02-19 09:14:19 -0500

    course interface

--- src/main/java/com/github/perscholas/service/CourseService.java
+++ src/main/java/com/github/perscholas/service/CourseService.java
@@ -2,9 +2,14 @@ package com.github.perscholas.service;
 
 import com.github.perscholas.DatabaseConnection;
 import com.github.perscholas.dao.CourseDao;
+import com.github.perscholas.model.Course;
 import com.github.perscholas.model.CourseInterface;
+import com.github.perscholas.model.Student;
+import com.github.perscholas.model.StudentInterface;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 // TODO - Implement respective DAO interface
@@ -23,6 +28,20 @@ public class CourseService implements CourseDao {
     @Override
     public List<CourseInterface> getAllCourses() {
         ResultSet result = dbc.executeQuery("SELECT * FROM courses");
+
+        List<CourseInterface> courseList = new ArrayList<>();
+        try {
+            while (result.next()) {
+                Integer id = result.getInt("id");
+                String name = result.getString("name");
+                String instructor = result.getString("instructor");
+                CourseInterface course = new Course(id, name, instructor);
+                courseList.add(course);
+            }
+        } catch(SQLException se) {
+            throw new Error(se);
+        }
+
 
         return null;
     }
